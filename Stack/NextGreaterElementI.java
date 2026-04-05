fe6c2d61ee3470d951d087e0d6430f1f4a52ecf3
hemnath-kandasamyk <hemnathkandasamyk@gmail.com>
2026-04-05 16:16:05 +0530

    Create NextGreaterElementI.java
    Problem No : 496 
    Problem Name :  Next Greater Element I
    
    **Approach:**  
    - Used Monotonic Decreasing Stack  
    - Precomputed next greater elements for nums2  
    - Stored results using HashMap  
    
    **Time Complexity:** O(n + m)  
    **Space Complexity:** O(n)
    
    **Concepts Used:**
    - Stack
    - Monotonic Stack
    - HashMap

---
+++ Stack/NextGreaterElementI.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+     HashMap<Integer,Integer> map=new HashMap<>();
+     Deque<Integer> stack=new ArrayDeque<>();
+     for(int i=nums2.length-1;i>=0;i--){
+        while(!stack.isEmpty() && stack.peek()<=nums2[i]){
+            stack.pop();
+        }
+        if(stack.isEmpty()){
+            map.put(nums2[i],-1);
+        }
+        else{
+            map.put(nums2[i],stack.peek());
+        }
+        stack.push(nums2[i]);
+     }
+     for(int i=nums1.length-1;i>=0;i--){
+        nums1[i]=map.get(nums1[i]);
+     }   
+     return nums1;
+    }
+}
